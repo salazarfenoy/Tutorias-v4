0be7338edf6d7ba022ad08467c0fead958bbb771
Jesús Salazar Fenoy <salazarfenoy@gmail.com>
2020-04-15 17:30:34 +0200

    Creación .css, .fxml, MainApp vista gráfica, paquetes.

---
+++ src/main/java/org/iesalandalus/programacion/tutorias/MainAppIUGrafica.java
@@ -0,0 +1,48 @@
+package org.iesalandalus.programacion.tutorias;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class MainAppIUGrafica extends Application {
+
+	double x, y;
+
+	private void addDragListeners(final Node n, Stage primaryStage){
+
+	    n.setOnMousePressed((MouseEvent mouseEvent) -> {
+	        this.x = n.getScene().getWindow().getX() - mouseEvent.getScreenX();
+	        this.y = n.getScene().getWindow().getY() - mouseEvent.getScreenY();
+	    });
+
+	    n.setOnMouseDragged((MouseEvent mouseEvent) -> {
+	        primaryStage.setX(mouseEvent.getScreenX() + this.x);
+	        primaryStage.setY(mouseEvent.getScreenY() + this.y);
+	    });
+	}
+	
+	@Override
+	public void start(Stage escenarioPrincipal) {
+		try {
+			BorderPane raiz = FXMLLoader.load(getClass().getResource("mvc/vista/grafica/vista/VentanaPrincipal.fxml"));
+			Scene escena = new Scene(raiz);
+			escenarioPrincipal.setScene(escena);
+			escenarioPrincipal.initStyle(StageStyle.UNDECORATED);
+			addDragListeners(raiz, escenarioPrincipal);
+			escenarioPrincipal.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
